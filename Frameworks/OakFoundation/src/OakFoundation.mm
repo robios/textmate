ec180d6ef1b41c86a46e337a1ebd5a9410dc3dac
tectiv3 <tectiv3@users.noreply.github.com>
2026-06-08 18:32:13 +0900

    Fix defaults observer deadlock risk
    Deliver NSUserDefaultsDidChangeNotification observers without making the posting thread wait on the main operation queue. The callback still runs on the main queue for existing UI-facing observers.
    
    Tune clang-format so saving Objective-C++ files does not create unreadable selector and assignment alignment columns.

--- Frameworks/OakFoundation/src/OakFoundation.mm
+++ Frameworks/OakFoundation/src/OakFoundation.mm
@@ -5,8 +5,9 @@ std::string OakMoveToTrash (std::string const& path)
 {
 	NSURL* resultingItemURL;
 	if([NSFileManager.defaultManager trashItemAtURL:[NSURL fileURLWithPath:[NSString stringWithCxxString:path]] resultingItemURL:&resultingItemURL error:nil])
-			return resultingItemURL.fileSystemRepresentation ?: NULL_STR;
-	else	return NULL_STR;
+		return resultingItemURL.fileSystemRepresentation ?: NULL_STR;
+	else
+		return NULL_STR;
 }
 
 BOOL OakIsEmptyString (NSString* str)
@@ -19,12 +20,24 @@ BOOL OakNotEmptyString (NSString* str)
 	return str && ![str isEqualToString:@""];
 }
 
-void OakObserveUserDefaults (id <OakUserDefaultsObserver> obj)
+void OakObserveUserDefaults (id<OakUserDefaultsObserver> obj)
 {
-	__weak id <OakUserDefaultsObserver> weakObject = obj;
-	__weak __block id token = [NSNotificationCenter.defaultCenter addObserverForName:NSUserDefaultsDidChangeNotification object:NSUserDefaults.standardUserDefaults queue:NSOperationQueue.mainQueue usingBlock:^(NSNotification* notification){
-		if(id <OakUserDefaultsObserver> strongObject = weakObject)
-				[strongObject userDefaultsDidChange:notification];
-		else	[NSNotificationCenter.defaultCenter removeObserver:token];
-	}];
+	__weak id<OakUserDefaultsObserver> weakObject = obj;
+	__weak __block id token;
+	void (^handler)(NSNotification*) = ^(NSNotification* notification) {
+		if(weakObject)
+		{
+			dispatch_async(dispatch_get_main_queue(), ^{
+				if(id<OakUserDefaultsObserver> strongObject = weakObject)
+					[strongObject userDefaultsDidChange:notification];
+				else
+					[NSNotificationCenter.defaultCenter removeObserver:token];
+			});
+		}
+		else
+		{
+			[NSNotificationCenter.defaultCenter removeObserver:token];
+		}
+	};
+	token = [NSNotificationCenter.defaultCenter addObserverForName:NSUserDefaultsDidChangeNotification object:NSUserDefaults.standardUserDefaults queue:nil usingBlock:handler];
 }
